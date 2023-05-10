6dc2a347ed863126b07139e893b345b02cff647d
Matheus Augusto Mendonça <97566776+Luerfel@users.noreply.github.com>
2023-05-10 15:55:04 -0300

    Add files via upload
    Adicionado 2 novos finais e um miniboss

---
+++ trelica.py
@@ -0,0 +1,6 @@
+import tkinter as tk
+
+
+Janela = tk.Tk() # container que contem o GUI da interface gráfica
+Janela.title("Calculo de trelicas") #Titulo Da Janela
+Janela.mainloop() # Loop Infinito que atualiza a interace gráfica

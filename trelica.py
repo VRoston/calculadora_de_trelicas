f573e636b04ac74eef654311795983d79682341e
Matheus Augusto Mendonça <97566776+Luerfel@users.noreply.github.com>
2023-05-12 16:28:17 -0300

    Update trelica.py

--- trelica.py
+++ trelica.py
@@ -1,23 +1,31 @@
-import tkinter as tk
+import tkinter as tk        # importa a biblioteca tkinter para usar com a API tk
 
-root = tk.Tk()
-canvas = tk.Canvas(root, width=500, height=300, bg='white')
-canvas.pack()
+root = tk.Tk()             # cria uma instância de Tk
 
-x0, y0 = 0, 0
-x1, y1 = 0, 0
+canvas = tk.Canvas(root, bg="white", width=600, height=400)   # cria uma instância de Canvas com o tamanho de 600x400 e com fundo branco
+canvas.pack()              # pack a janela, adicionando o canvas
 
-def on_first_right_click(event):
-    global x0, y0, x1, y1
-    x0, y0 = event.x, event.y
-    canvas.create_line(0, 0, x0, y0, fill="red")
+coords = {"x":0,"y":0,"x2":0,"y2":0}    # define uma lista para armazenar as coordenadas das linhas
+# mantém uma referência a todas as linhas mantendo-as em uma lista
+lines = []
 
-def on_second_right_click(event):
-    global x1, y1
-    x1, y1 = event.x, event.y
-    canvas.create_line(x1, y1, 0, 0, fill="black")
+def click(e):
+    # define o ponto inicial da linha
+    coords["x"] = e.x
+    coords["y"] = e.y
 
-canvas.bind('<Button-1>', on_first_right_click)
-canvas.bind('<Button-3>', on_second_right_click)
+    # cria uma linha nesse ponto e a armazena na lista
+    lines.append(canvas.create_line(coords["x"],coords["y"],coords["x"],coords["y"]))
 
-root.mainloop()
+def drag(e):
+    # atualize as coordenadas a partir do evento
+    coords["x2"] = e.x
+    coords["y2"] = e.y
+
+    # mude as coordenadas da ultima linha criada para as coordenadas novas
+    canvas.coords(lines[-1], coords["x"], coords["y"], coords["x2"], coords["y2"])
+
+canvas.bind("<ButtonPress-1>", click)            # ligação da função click para o evento ButtonPress-1
+canvas.bind("<B1-Motion>", drag)                  # ligação da função drag para oevents B1-Motion
+
+root.mainloop()                                # inicia o loop principal

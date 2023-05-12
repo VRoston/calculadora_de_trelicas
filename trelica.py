1064cd3de30a0d481639290454f2b4683ef6947f
Matheus Augusto Mendonça <97566776+Luerfel@users.noreply.github.com>
2023-05-12 17:10:10 -0300

    Update trelica.py

--- trelica.py
+++ trelica.py
@@ -1,21 +1,20 @@
 import tkinter as tk        # importa a biblioteca tkinter para usar com a API tk
 
-root = tk.Tk()             # cria uma instância de Tk
+root = tk.Tk()             # cria uma janela
 
-canvas = tk.Canvas(root, bg="white", width=600, height=400)   # cria uma instância de Canvas com o tamanho de 600x400 e com fundo branco
+canvas = tk.Canvas(root, bg="white", width=1200, height=800)   # cria uma instância de Canvas com o tamanho de 600x400 e com fundo branco
 canvas.pack()              # pack a janela, adicionando o canvas
 
 coords = {"x":0,"y":0,"x2":0,"y2":0}    # define uma lista para armazenar as coordenadas das linhas
 # mantém uma referência a todas as linhas mantendo-as em uma lista
 lines = []
-
 def click(e):
     # define o ponto inicial da linha
     coords["x"] = e.x
     coords["y"] = e.y
 
     # cria uma linha nesse ponto e a armazena na lista
-    lines.append(canvas.create_line(coords["x"],coords["y"],coords["x"],coords["y"]))
+    lines.append(canvas.create_line(coords["x"],coords["y"],coords["x"],coords["y"], width="5")) #cria a linha
 
 def drag(e):
     # atualize as coordenadas a partir do evento
@@ -25,7 +24,7 @@ def drag(e):
     # mude as coordenadas da ultima linha criada para as coordenadas novas
     canvas.coords(lines[-1], coords["x"], coords["y"], coords["x2"], coords["y2"])
 
-canvas.bind("<ButtonPress-1>", click)            # ligação da função click para o evento ButtonPress-1
-canvas.bind("<B1-Motion>", drag)                  # ligação da função drag para oevents B1-Motion
+canvas.bind("<ButtonPress-1>", click)            
+canvas.bind("<B1-Motion>", drag)                  
 
-root.mainloop()                                # inicia o loop principal
+root.mainloop()                                # deixa o programa aberto

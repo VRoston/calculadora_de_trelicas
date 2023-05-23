f3f96ac1a4bad9f72c35c245061bdf57e2e4f22e
GolfBravoSierra <106974254+GolfBravoSierra@users.noreply.github.com>
2023-05-23 09:19:04 -0300

    testes

---
+++ salvando_codigos.py
@@ -0,0 +1,10 @@
+a = {}
+k = 0
+while k < 10:
+    # dynamically create key
+    key = ...
+    # calculate value
+    value = ...
+    a[key] = value 
+    k += 1
+    # criei essa pasata só para salvar os codigos que eu achei  na internet e que podem ser uteis no futuro
--- trelica.py
+++ trelica.py
@@ -22,25 +22,42 @@ entrada_vigas.grid(column=1, row=1)                     # define a localização
 botão_confirmar = tk.Button(root, text="OK", command=lambda: botão_confirmar()) # usa a função Button para criar um botão 
 botão_confirmar.grid(column=0, row=2)
 
+posição_x = []                                          # cria uma lista para armazenar as posições X dos nós
+posição_y = []                                          # cria uma lista para armazenar as posições Y dos nós
+contador_de_linhas = 4                                  # váriavel auxiliar para imprimir texto 
+                                          
+
 def botão_confirmar():                                  # Função do botão 
-    entrada_nos_int = int(entrada_nos.get())                 
-    entrada_vigas_int = int(entrada_vigas.get())        # transformar as duas varias entradavigas e entradanos no tipo INT
-    linhas = 4                                          # váriavel auxiliar para imprimir texto 
-    int (linhas)
+    entrada_nos_int = int(entrada_nos.get())            # transformar a variavel entradanos no tipo INT
+    linhas = int (contador_de_linhas)                                        # transformar a variavel linhas no tipo INT
     
     for i in range(entrada_nos_int):
         linhas = linhas + 1
-        label = tk.Label (root , text = "posição X do nó " + str(i+1) + ":")      
-        label.grid (column = 0, row = linhas)                       # Impressão do texto da posição X
-        entrada_vigas_int = tk.Entry (root ,width=10)               # cria uma caixa de entrada do tamanho 10
-        entrada_vigas_int.grid(column=1, row=linhas)  
-    
-        label = tk.Label (root , text = "posição Y do nó " + str(i+1) + ":")
-        label.grid (column = 0, row = linhas+1)
-        entrada_vigas_int2 = tk.Entry (root ,width=10)               # cria uma caixa de entrada do tamanho 10
-        entrada_vigas_int2.grid(column=1, row=linhas+1)  
+        label = tk.Label (root , text = "posição X e Y do nó " + str(i+1) + ":")      
+        label.grid (column = 0, row = linhas)            # Impressão do texto da posição X
+        posição_x = tk.Entry (root ,width=10)            # cria uma caixa de entrada do tamanho 10
+        posição_x.grid(column=1, row=linhas)             # para a posição X do nó 
+        posição_y = tk.Entry (root ,width=10)            # cria uma caixa de entrada do tamanho 10
+        posição_y.grid(column=2, row=linhas)             # para a posição Y do nó
         
+        
+    botão_confirmar = tk.Button(root, text="proximo", command=lambda: botão_proximo()) # usa a função Button para criar um botão 
+    botão_confirmar.grid(column=0, row=linhas+1)
+    return
+def botão_proximo():                                     # Função do botão 
+    entrada_nos_int = int(entrada_nos.get())                 
+    entrada_vigas_int = int(entrada_vigas.get())         # transformar as duas varias entradavigas e entradanos no tipo INT                                          # váriavel auxiliar para imprimir texto 
+    linhas = int (contador_de_linhas) 
+    
+    for i in range(entrada_vigas_int):
+        linhas = linhas + 1
+        label = tk.Label (root , text = "posição X e Y do nó " + str(i+1) + ":")      
+        label.grid (column = 0, row = linhas)            # Impressão do texto da posição X
+        posição_x = tk.Entry (root ,width=10)            # cria uma caixa de entrada do tamanho 10
+        posição_x.grid(column=1, row=linhas)             # para a posição X do nó 
+        posição_y = tk.Entry (root ,width=10)            # cria uma caixa de entrada do tamanho 10
+        posição_y.grid(column=2, row=linhas)             # para a posição Y do nó
+    
     return
 
-
-root.mainloop()                                         # deixa o programa aberto
+root.mainloop()                                          # deixa o programa aberto

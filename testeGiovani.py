3e2f334d2e7f8b5e6c785a06fb5e454113011f39
GolfBravoSierra <106974254+GolfBravoSierra@users.noreply.github.com>
2023-05-28 16:16:36 -0300

    comentarios

--- testeGiovani.py
+++ testeGiovani.py
@@ -17,10 +17,10 @@ root.geometry(winsize)
 
 
 
-v_posição_x = []
-v_posição_x.clear()
-v_posição_y = []
-v_posição_y.clear()
+v_posição_x = []                                                     # cria uma lista para armazenar as posições X dos nós
+v_posição_x.clear()                                                  # limpa a lista v_posição_x
+v_posição_y = []                                                     # cria uma lista para armazenar as posições Y dos nós
+v_posição_y.clear()                                                  # limpa a lista v_posição_y
 
 
 
@@ -39,49 +39,45 @@ entrada_vigas.grid(column=1, row=1)                                  # define a 
 botão_confirmar = tk.Button(root, text="proximo", command=lambda: botão_confirmar()) # usa a função Button para criar um botão 
 botão_confirmar.grid(column=0, row=2)
 
-lista_de_entradanos_x = []                                             # cria uma lista para armazenar os nós
-lista_de_entradanos_y = []                                             # cria uma lista para armazenar os nós
-contador_de_linhas = int(4)                                          # cria uma lista para armazenar as barras
+lista_de_entradanos_x = []                                           # cria uma lista para armazenar os nós
+lista_de_entradanos_y = []                                           # cria uma lista para armazenar os nós
+contador_de_linhas = int(4)                                          # variavel usada para contar linhas e serveri de base para onde imprimir os dados na tela 
 cordenadas_das_barras = []                                           # cria uma lista para armazenar as cordenadas das barras
 
 
 def botão_confirmar():      
-    entrada_nos_int = int(entrada_nos.get())                         # transformar a variavel entradanos no tipo INT
-    linhas = int (contador_de_linhas) 
-    #global v_posição_x                                                 # cria uma lista para armazenar as posições X dos nós  
-    #global v_posição_y                                                 # cria uma lista para armazenar as posições Y dos nós 
-    
+    entrada_nos_int = int(entrada_nos.get())                         # transformar a variavel entradanos no tipo INT e armazenar na variavel entrada_nos_int para ser usada na função
+    linhas = int (contador_de_linhas)                                # repasando o valor de contador_de_linhas para a variavel linhas para ser usado na função
                                  
-    for i in range(entrada_nos_int):
-        linhas = linhas + 1
+    for i in range(entrada_nos_int):                                 # for para criar as caixas de entrada de dados dos nós
+        linhas = linhas + 1                                          #adicinado 1 a variavel linhas para pular uma linha na tela ante de imprimir a mensagem
         label = tk.Label (root , text = "posição X e Y do nó " + str(i+1) + ":")      
         label.grid (column = 0, row =linhas)   
-        #ntrada_x = tk.Entry()                       # cria uma caixa de entrada do tamanho 10
-        caixa = tk.Entry (root ,width=10)                              # cria uma caixa de entrada do tamanho 10
-        caixa.grid(column=1, row=linhas)
-        lista_de_entradanos_x.append(caixa) # adiciona as posições X dos nós na lista de entrada de nós
+        
+        caixa = tk.Entry (root ,width=10)                            # cria uma caixa da posição x dos nós
+        caixa.grid(column=1, row=linhas)                             # marcando onde ele vai ser impressa na tela   
+        lista_de_entradanos_x.append(caixa)                          # adicinando o valor que foi inserido na caixa de entrada a uma lista para salvar as cordenadas dos nós
             
-                                        # adiciona as posições X dos nós na lista v_posição_x         
-        caixa = tk.Entry (root ,width=10)                              # cria uma caixa de entrada do tamanho 10
-        caixa.grid(column=2, row=linhas)
-        lista_de_entradanos_y.append(caixa) 
+                                                
+        caixa = tk.Entry (root ,width=10)                            # cria uma caixa da posição x dos nós
+        caixa.grid(column=2, row=linhas)                             # marcando onde ele vai ser impressa na tela
+        lista_de_entradanos_y.append(caixa)                          # adicinando o valor que foi inserido na caixa de entrada a uma lista para salvar as cordenadas dos nós
                                        
                
-    botão_confirmar = tk.Button(root, text="proximo", command=lambda: botão_proximo()) # usa a função Button para criar um botão 
+    botão_confirmar = tk.Button(root, text="proximo", command=lambda: botão_proximo()) # usa a função Button para criar um botão que ativara a proxima etapa do programa que é pegar as cordenadas das barras
     botão_confirmar.grid(column=0, row=linhas+1)
     
-    botão_teste = tk.Button(root, text="teste", command=lambda: teste() ) # usa a função Button para criar um botão 
+    botão_teste = tk.Button(root, text="teste", command=lambda: teste() ) # usa a função Button para criar um botão de teste 
     botão_teste.grid(column=3, row=linhas+1)
     
-    return linhas , v_posição_x , v_posição_y
+    return linhas                                                    # retorna o valor da variavel linhas para ser usado na função botão_proximo
 
 def teste():
-    global v_posição_x                                                 # cria uma lista para armazenar as posições X dos nós  
-    global v_posição_y 
-    tamanho_lista = len(lista_de_entradanos_x) #len é uma função que pega o tamanho da lista 
-    print (lista_de_entradanos_x)
+    global v_posição_x                                               # cria uma lista para armazenar as posições X dos nós  
+    global v_posição_y                                               # cria uma lista para armazenar as posições Y dos nós
+    tamanho_lista = len(lista_de_entradanos_x)                       # len é uma função que pega o tamanho da lista para ser usado no for 
+    print (lista_de_entradanos_x)                                    # printa a lista de entrada dos nós como teste
     for i in range(tamanho_lista):
-        #print (v_posição_x[i])
         v_posição_x.append(int(lista_de_entradanos_x[i].get()))      # esse funciona para pegar o valor da caixa de entrada
         v_posição_y.append(int(lista_de_entradanos_y[i].get()))      # esse funciona para pegar o valor da caixa de entrada
         print (v_posição_x[i])
